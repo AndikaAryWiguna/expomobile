cce353e676a3c0d8e4e5576ec8a949f99c163310
DodyDharma440 <dharma.dody@yahoo.com>
2025-09-21 21:07:45 +0700

    update category

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,9 +1,8 @@
-// src/data/categories.ts
 import { Category } from "../types";
 
 export const initialCategories: Category[] = [
-  { name: "Hot Coffee", icon: "☕", isSelected: true },
-  { name: "Big Burger", icon: "🍔", isSelected: false },
-  { name: "Ice Coffee", icon: "🧊", isSelected: false },
-  { name: "Pastry", icon: "🍰", isSelected: false },
+  { name: "Hot Coffee", icon: require("../../assets/images/hotcoffe.png"), isSelected: true },
+  { name: "Big Burger", icon: require("../../assets/images/burger.png"), isSelected: false },
+  { name: "Ice Coffee", icon: require("../../assets/images/icecoffe.png"), isSelected: false },
+  { name: "Pastry", icon: require("../../assets/images/pastry.png"), isSelected: false },
 ];
